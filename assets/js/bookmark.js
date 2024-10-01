d3d99c2828858340e5b4223451a09236f3db6f6b
seonsssss <gkstjs9811@daum.net>
2024-10-01 18:29:40 +0900

    즐겨찾기 movie이동

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -1,20 +1,40 @@
-import { isLoggedIn } from "./login.js";
-document.addEventListener("DOMContentLoaded", function () {
-  const bookmarkBefore = document.querySelector(".bookmarkBefore");
-  const bookmarkAfter = document.querySelector(".bookmarkAfter");
-
+import { checkLoginStatus, logintoggle } from "./login.js";
+const bookmarkBefore = document.querySelector(".bookmarkBefore");
+const bookmarkAfter = document.querySelector(".bookmarkAfter");
+window.onload = function () {
   updateBookmarkVisibility();
+  recentlyViewedPlus();
+  checkLoginStatus();
+  logintoggle();
+  console.log("recentlyViewed:" + localStorage.getItem("recentlyViewedMovies"));
+  console.log("loggedIn:" + localStorage.getItem("loggedIn"));
+};
 
-  function updateBookmarkVisibility() {
-    const isLoggedIn = localStorage.getItem("loggedIn");
-
-    if (isLoggedIn === "true") {
-      bookmarkBefore.style.display = "none";
-      bookmarkAfter.style.display = "block";
-    } else {
-      bookmarkBefore.style.display = "block";
-      bookmarkAfter.style.display = "none";
-    }
+function updateBookmarkVisibility() {
+  const isLoggedIn = localStorage.getItem("loggedIn");
+  if (isLoggedIn === "true") {
+    bookmarkBefore.style.display = "none";
+    bookmarkAfter.style.display = "block";
+  } else {
+    bookmarkBefore.style.display = "block";
+    bookmarkAfter.style.display = "none";
   }
+}
+
+function recentlyViewedPlus() {
+  const recentlyViewedDiv = document.querySelector(".recentlyViewedMoviesDiv");
+  const recentlyViewed = JSON.parse(localStorage.getItem("recentlyViewedMovies")) || [];
+
+  recentlyViewed.forEach((movie) => {
+    console.log("recentlyViewed:", movie);
+    const movieHTML = `
+      <div class="recent-movie">
+        <img src="${movie.image}" alt="${movie.title}">
+        <p>${movie.title}</p>
+      </div>
+    `;
+    recentlyViewedDiv.innerHTML += movieHTML; // Append the movie HTML to the div
+  });
+
   
-});
+}
--- assets/js/movie.js
+++ assets/js/movie.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   checkLoginStatus();
   logintoggle();
   const movieList = document.getElementById("movieList");
+  let recentlyViewed = localStorage.getItem("recentlyViewed");
   movieList.addEventListener("click", function (event) {
     // love 클래스가 포함된 이미지인지 확인
     if (event.target && event.target.closest(".love img")) {
@@ -13,6 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
           heart.src = "assets/images/movie/하트.png"; // 채워진 하트 이미지
           heart.classList.remove("heart-empty");
           heart.classList.add("heart-full");
+
+          const movieItem = heart.closest(".movie-item");
+          const movieId = movieItem.id.replace("movie", "");
+          const movieDetails = {
+            movieId: movieId,
+            title: movieItem.querySelector(".text p").textContent,
+            image: movieItem.querySelector(".movie-img").src,
+          };
+          let recentlyViewed = JSON.parse(localStorage.getItem("recentlyViewedMovies")) || [];
+          recentlyViewed.push(movieDetails);
+          localStorage.setItem("recentlyViewedMovies", JSON.stringify(recentlyViewed));
+          console.log("movieDetails" + movieDetails);
+          console.log("recentlyViewed" + recentlyViewed);
         } else {
           heart.src = "assets/images/movie/빈하트.png"; // 빈 하트 이미지
           heart.classList.remove("heart-full");
@@ -29,93 +43,92 @@ document.addEventListener("DOMContentLoaded", function () {
     return urlParams.get("movieId");
   }
 
-    const movies = {
-      1: {
-        movietxtSub: "★과일빙수송★ | 무더운 여름엔 시원한 과일 빙수",
-        videoPath: "assets/video/과일빙수송.mp4",
-        image: "assets/images/movie/과일빙수송.png",
-        time: "12:00",
-      },
-      2: {
-        movietxtSub: "신나는 캠프",
-        videoPath: "assets/video/신나는 캠프.mp4",
-        image: "assets/images/movie/신나는 캠프 ♪.png",
-        time: "12:00",
-      },
-      3: {
-        movietxtSub: "아기상어 숫자놀이",
-        videoPath: "assets/video/아기상어 숫자놀이.mp4",
-        image: "assets/images/movie/아기상어 숫자 놀이.jpg",
-        time: "12:00",
-      },
-      4: {
-        movietxtSub: "응가송",
-        videoPath: "assets/video/응가송.mp4",
-        image: "assets/images/movie/응가송.jpg",
-        time: "12:00",
-      },
-      5: {
-        movietxtSub: "정글브라운 15화",
-        videoPath: "assets/video/정글브라운 15화.mp4",
-        image: "assets/images/movie/정글브라운 15화.jpg",
-        time: "12:00",
-      },
-      6: {
-        movietxtSub: "코리리동요",
-        videoPath: "assets/video/코리리동요.mp4",
-        image: "assets/images/movie/코리리동요.png",
-        time: "12:00",
-      },
-      7: {
-        movietxtSub: "트니프렌즈! 베니의 생활습관",
-        videoPath: "assets/video/트니프렌즈! 베니의 생활습관.mp4",
-        image: "assets/images/movie/트니프렌즈! 베니의 생활습관.png",
-        time: "12:00",
-      },
-      8: {
-        movietxtSub: "페티의 여름",
-        videoPath: "assets/video/페티의 여름.mp4",
-        image: "assets/images/movie/페티의 여름.jpeg",
-        time: "12:00",
-      },
-      9: {
-        movietxtSub: "감자도리",
-        videoPath: "assets/video/도리도리 감자도리.mp4",
-        image: "assets/images/movie/도리도리송(감자도리송).jpg",
-        time: "12:00",
-      },
-      10: {
-        movietxtSub: "나의 작은 주전자",
-        videoPath: "assets/video/나의 작은 주전자.mp4",
-        image: "assets/images/movie/나는 작은 주전자.png",
-        time: "12:00",
-      },
-      11: {
-        movietxtSub: "★과일빙수송★ | 무더운 여름엔 시원한 과일 빙수",
-        videoPath: "assets/video/프랑키의 다이빙.mp4",
-        image: "assets/images/movie/[스푸키즈 쿠키] 7화  프랑키의 다이빙.jpg",
-        time: "12:00",
-      },
-      12: {
-        movietxtSub: "뽀득뽀득 세차해요",
-        videoPath: "assets/video/뽀득뽀득 세차해요.mp4",
-        image: "assets/images/movie/아기상어 자동차와 뽀득뽀득 세차해요.jpg",
-        time: "12:00",
-      },
-    };
+  let movies = {
+    1: {
+      movietxtSub: "★과일빙수송★ | 무더운 여름엔 시원한 과일 빙수",
+      videoPath: "assets/video/과일빙수송.mp4",
+      image: "assets/images/movie/과일빙수송.png",
+      time: "12:00",
+    },
+    2: {
+      movietxtSub: "신나는 캠프",
+      videoPath: "assets/video/신나는 캠프.mp4",
+      image: "assets/images/movie/신나는 캠프 ♪.png",
+      time: "12:00",
+    },
+    3: {
+      movietxtSub: "아기상어 숫자놀이",
+      videoPath: "assets/video/아기상어 숫자놀이.mp4",
+      image: "assets/images/movie/아기상어 숫자 놀이.jpg",
+      time: "12:00",
+    },
+    4: {
+      movietxtSub: "응가송",
+      videoPath: "assets/video/응가송.mp4",
+      image: "assets/images/movie/응가송.jpg",
+      time: "12:00",
+    },
+    5: {
+      movietxtSub: "정글브라운 15화",
+      videoPath: "assets/video/정글브라운 15화.mp4",
+      image: "assets/images/movie/정글브라운 15화.jpg",
+      time: "12:00",
+    },
+    6: {
+      movietxtSub: "코리리동요",
+      videoPath: "assets/video/코리리동요.mp4",
+      image: "assets/images/movie/코리리동요.png",
+      time: "12:00",
+    },
+    7: {
+      movietxtSub: "트니프렌즈! 베니의 생활습관",
+      videoPath: "assets/video/트니프렌즈! 베니의 생활습관.mp4",
+      image: "assets/images/movie/트니프렌즈! 베니의 생활습관.png",
+      time: "12:00",
+    },
+    8: {
+      movietxtSub: "페티의 여름",
+      videoPath: "assets/video/페티의 여름.mp4",
+      image: "assets/images/movie/페티의 여름.jpeg",
+      time: "12:00",
+    },
+    9: {
+      movietxtSub: "감자도리",
+      videoPath: "assets/video/도리도리 감자도리.mp4",
+      image: "assets/images/movie/도리도리송(감자도리송).jpg",
+      time: "12:00",
+    },
+    10: {
+      movietxtSub: "나의 작은 주전자",
+      videoPath: "assets/video/나의 작은 주전자.mp4",
+      image: "assets/images/movie/나는 작은 주전자.png",
+      time: "12:00",
+    },
+    11: {
+      movietxtSub: "★과일빙수송★ | 무더운 여름엔 시원한 과일 빙수",
+      videoPath: "assets/video/프랑키의 다이빙.mp4",
+      image: "assets/images/movie/[스푸키즈 쿠키] 7화  프랑키의 다이빙.jpg",
+      time: "12:00",
+    },
+    12: {
+      movietxtSub: "뽀득뽀득 세차해요",
+      videoPath: "assets/video/뽀득뽀득 세차해요.mp4",
+      image: "assets/images/movie/아기상어 자동차와 뽀득뽀득 세차해요.jpg",
+      time: "12:00",
+    },
+  };
 
-
-    function displayMovieDetails(movieId) {
-      const movie = movies[movieId];
-      if (movie) {
-        document.getElementById("movie-video").src = movie.videoPath;
-        document.getElementById("movietxtSub").innerHTML = movie.movietxtSub;
-      } else {
-        document.getElementById("movietxtSub").innerHTML = `
+  function displayMovieDetails(movieId) {
+    const movie = movies[movieId];
+    if (movie) {
+      document.getElementById("movie-video").src = movie.videoPath;
+      document.getElementById("movietxtSub").innerHTML = movie.movietxtSub;
+    } else {
+      document.getElementById("movietxtSub").innerHTML = `
           <p>Movie not found.</p>
         `;
-      }
     }
+  }
 
   function renderMovieList() {
     const movieList = document.getElementById("movieList");
@@ -154,4 +167,12 @@ document.addEventListener("DOMContentLoaded", function () {
   if (movieId) {
     displayMovieDetails(movieId);
   }
+
+  // function getRecentlyViewed() {
+  //   return JSON.parse(localStorage.getItem("recentlyViewedMovies")) || [];
+  // }
+
+  // function setRecentlyViewed(recentlyViewed) {
+  //   localStorage.setItem("recentlyViewedMovies", JSON.stringify(recentlyViewed));
+  // }
 });
