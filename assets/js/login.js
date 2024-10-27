6956484afbd285d26eba07080ae580fb2f906bfe
seonsssss <gkstjs9811@daum.net>
2024-10-27 23:29:58 +0900

    로그인 키 구현 및 movie.json화

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,17 +2,17 @@ const login = document.querySelector(".login");
 const logout = document.querySelector(".logout");
 let isLoggedIn = localStorage.getItem("loggedIn");
 
-// 페이지 로드 시 로그인 상태 확인
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
   checkLoginStatus();
   logintoggle();
-  console.log("loggedIn:" + localStorage.getItem("loggedIn")); 
-};
+  checkLoginForm();
+  console.log("loggedIn:", localStorage.getItem("loggedIn"));
+});
 
-// 로그인 상태 확인 함수
 function checkLoginStatus() {
   isLoggedIn = localStorage.getItem("loggedIn");
-
+  
+  if(login || logout) {
   if (isLoggedIn === "true") {
     login.style.display = "none";
     logout.style.display = "block";
@@ -21,19 +21,44 @@ function checkLoginStatus() {
     logout.style.display = "none";
   }
 }
+}
+function checkLoginForm() {
+  const loginForm = document.getElementById("loginForm");
 
-function logintoggle() {
-login.addEventListener("click", function () {
-  localStorage.setItem("loggedIn", "true");
-  checkLoginStatus();
-  window.location.reload();
-});
+  if (loginForm) {
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
 
-logout.addEventListener("click", function () {
-  localStorage.setItem("loggedIn", "false");
-  checkLoginStatus();
-  window.location.reload();
-});
+      const userId = document.getElementById("userId").value;
+      const userPassword = document.getElementById("userPassword").value;
+
+      const validUserId = "user";
+      const validUserPassword = "password";
+
+      if (userId === validUserId && userPassword === validUserPassword) {
+        localStorage.setItem("loggedIn", "true"); 
+        checkLoginStatus();
+        window.location.href = "home.html";
+
+      } else {
+        alert("로그인 실패: 아이디 또는 비밀번호가 잘못되었습니다.");
+      }
+    });
+  }
 }
 
-export { isLoggedIn, checkLoginStatus, logintoggle};
+function logintoggle() {
+  if(login || logout) {
+  login.addEventListener("click", function () {
+    checkLoginStatus();
+  });
+
+  logout.addEventListener("click", function () {
+    localStorage.setItem("loggedIn", "false");
+    checkLoginStatus();
+    window.location.reload();
+  });
+}
+}
+
+export { isLoggedIn, checkLoginStatus, logintoggle };
--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,208 +1,103 @@
-import { isLoggedIn, checkLoginStatus, logintoggle } from "./login.js";
-import { movies } from "./movies.js";
+import { checkLoginStatus, logintoggle } from "./login.js";
+// import { movies } from "./movies.js";
+
 const movieList = document.getElementById("movieList");
-let isMovieAlreadyLiked;
 let likeMovieList = JSON.parse(localStorage.getItem("likeMovieList")) || [];
 
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", async function() {
   checkLoginStatus();
   logintoggle();
-  renderMovieList();
+  await loadMovies(); 
+  restoreLikedStatus(); 
+  setupHeartClickEvent(); 
+});
+async function loadMovies() {
+  try {
+    const response = await fetch("assets/data/movie.json");
+    const data = await response.json();
+    const movies = data.movies;
+    renderMovieList(movies);
+  } catch (error) {
+    console.error("Error loading movies:", error);
+  }
+}
+
+function setupHeartClickEvent() {
   movieList.addEventListener("click", function (event) {
-    // love 클래스가 포함된 이미지인지 확인
     if (event.target && event.target.closest(".love img")) {
       const heart = event.target;
       const movieItem = heart.closest(".movie-item");
       const movieId = movieItem.id.replace("movie", "");
+      const isLoggedIn = localStorage.getItem("loggedIn") === "true";
       const movieDetails = {
         movieId: movieId,
         title: movieItem.querySelector(".text p").textContent,
         image: movieItem.querySelector(".movie-img").src,
         isLiked: true,
       };
-      isMovieAlreadyLiked = likeMovieList.some(
-        (movie) => movie.movieId === movieId
-      );
-      console.log("isMovieAlreadyLiked:" + isMovieAlreadyLiked);
-      if (isLoggedIn === "true") {
+
+      if (isLoggedIn) {
         if (heart.classList.contains("heart-empty")) {
-          heart.src = "assets/images/movie/하트.png"; // 채워진 하트 이미지
+          heart.src = "assets/images/movie/하트.png";
           heart.classList.remove("heart-empty");
           heart.classList.add("heart-full");
-          if (!isMovieAlreadyLiked) {
-            likeMovieList.push(movieDetails); // 중복이 아닐 경우에만 추가
-            localStorage.setItem(
-              "likeMovieList",
-              JSON.stringify(likeMovieList)
-            ); // 업데이트된 리스트 저장
-            console.log("movieDetails:", movieDetails);
-            console.log("likeMovieList:", likeMovieList);
-          } else {
-            console.log("This movie is already in the bookmark list.");
-          }
+          likeMovieList.push(movieDetails);
+          localStorage.setItem("likeMovieList", JSON.stringify(likeMovieList));
         } else {
-          heart.src = "assets/images/movie/빈하트.png"; // 빈 하트 이미지
+          heart.src = "assets/images/movie/빈하트.png";
           heart.classList.remove("heart-full");
           heart.classList.add("heart-empty");
           likeMovieList = likeMovieList.filter((movie) => movie.movieId !== movieId);
-          localStorage.setItem("likeMovieList", JSON.stringify(likeMovieList)); // Update localStorage
-          console.log("Movie removed from likeMovieList:", movieId);
+          localStorage.setItem("likeMovieList", JSON.stringify(likeMovieList));
         }
       } else {
         const ask = confirm("네이버 로그인 하신 후 이용해 주시기 바랍니다.");
-        if (ask) {
-          window.location.href = "login.html";
-        } else {
-          return;
-        }
+        if (ask) window.location.href = "login.html";
       }
     }
   });
-  restoreLikedStatus();
-};
+}
 
 function restoreLikedStatus() {
+  const isLoggedIn = localStorage.getItem("loggedIn") === "true";
   likeMovieList.forEach((likedMovie) => {
-    const likedMovieItem = document.getElementById(
-      `movie${likedMovie.movieId}`
-    );
+    const likedMovieItem = document.getElementById(`movie${likedMovie.movieId}`);
     if (likedMovieItem) {
       const heart = likedMovieItem.querySelector(".love img");
-      if (heart) {
-        heart.src = "assets/images/movie/하트.png"; // Change to filled heart
+      if (isLoggedIn) {
+        heart.src = "assets/images/movie/하트.png";
         heart.classList.remove("heart-empty");
         heart.classList.add("heart-full");
+      } else {
+        heart.src = "assets/images/movie/빈하트.png";
+        heart.classList.add("heart-empty");
+        heart.classList.remove("heart-full");
       }
     }
   });
 }
 
-function getMovieIdFromURL() {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get("movieId");
-}
-
-// export const movies = {
-//   1: {
-//     movietxtSub: "★과일빙수송★ | 무더운 여름엔 시원한 과일 빙수",
-//     videoPath: "assets/video/과일빙수송.mp4",
-//     image: "assets/images/movie/과일빙수송.png",
-//     time: "12:00",
-//   },
-//   2: {
-//     movietxtSub: "신나는 캠프",
-//     videoPath: "assets/video/신나는 캠프.mp4",
-//     image: "assets/images/movie/신나는 캠프 ♪.png",
-//     time: "12:00",
-//   },
-//   3: {
-//     movietxtSub: "아기상어 숫자놀이",
-//     videoPath: "assets/video/아기상어 숫자놀이.mp4",
-//     image: "assets/images/movie/아기상어 숫자 놀이.jpg",
-//     time: "12:00",
-//   },
-//   4: {
-//     movietxtSub: "응가송",
-//     videoPath: "assets/video/응가송.mp4",
-//     image: "assets/images/movie/응가송.jpg",
-//     time: "12:00",
-//   },
-//   5: {
-//     movietxtSub: "정글브라운 15화",
-//     videoPath: "assets/video/정글브라운 15화.mp4",
-//     image: "assets/images/movie/정글브라운 15화.jpg",
-//     time: "12:00",
-//   },
-//   6: {
-//     movietxtSub: "코리리동요",
-//     videoPath: "assets/video/코리리동요.mp4",
-//     image: "assets/images/movie/코리리동요.png",
-//     time: "12:00",
-//   },
-//   7: {
-//     movietxtSub: "트니프렌즈! 베니의 생활습관",
-//     videoPath: "assets/video/트니프렌즈! 베니의 생활습관.mp4",
-//     image: "assets/images/movie/트니프렌즈! 베니의 생활습관.png",
-//     time: "12:00",
-//   },
-//   8: {
-//     movietxtSub: "페티의 여름",
-//     videoPath: "assets/video/페티의 여름.mp4",
-//     image: "assets/images/movie/페티의 여름.jpeg",
-//     time: "12:00",
-//   },
-//   9: {
-//     movietxtSub: "감자도리",
-//     videoPath: "assets/video/도리도리 감자도리.mp4",
-//     image: "assets/images/movie/도리도리송(감자도리송).jpg",
-//     time: "12:00",
-//   },
-//   10: {
-//     movietxtSub: "나의 작은 주전자",
-//     videoPath: "assets/video/나의 작은 주전자.mp4",
-//     image: "assets/images/movie/나는 작은 주전자.png",
-//     time: "12:00",
-//   },
-//   11: {
-//     movietxtSub: "★과일빙수송★ | 무더운 여름엔 시원한 과일 빙수",
-//     videoPath: "assets/video/프랑키의 다이빙.mp4",
-//     image: "assets/images/movie/[스푸키즈 쿠키] 7화  프랑키의 다이빙.jpg",
-//     time: "12:00",
-//   },
-//   12: {
-//     movietxtSub: "뽀득뽀득 세차해요",
-//     videoPath: "assets/video/뽀득뽀득 세차해요.mp4",
-//     image: "assets/images/movie/아기상어 자동차와 뽀득뽀득 세차해요.jpg",
-//     time: "12:00",
-//   },
-// };
-
-function displayMovieDetails(movieId) {
-  const movie = movies[movieId];
-  if (movie) {
-    document.getElementById("movie-video").src = movie.videoPath;
-    document.getElementById("movietxtSub").innerHTML = movie.movietxtSub;
-  } else {
-    document.getElementById("movietxtSub").innerHTML = `
-          <p>Movie not found.</p>
-        `;
-  }
-}
-
-function renderMovieList() {
-  const movieList = document.getElementById("movieList");
-  movieList.innerHTML = ""; // 기존 내용 초기화
-
-  Object.keys(movies).forEach((movieId) => {
-    const movie = movies[movieId];
-
+function renderMovieList(movies) {
+  movieList.innerHTML = ""; 
+  movies.forEach((movie) => {
     const movieItem = `
-          <div class="movie-item" id="movie${movieId}">
-            <div class="movie">
-              <a href="movie_sub.html?movieId=${movieId}">
-                <img src="${movie.image}" alt="${movie.movietxtSub}" class="movie-img">
-              </a>
-              <div class="movie-time">${movie.time}</div>
-            </div>
-            <div class="text-love">
-              <div class="text">
-                <p>${movie.movietxtSub}</p>
-              </div>
-              <div class="love">
-                <img src="assets/images/movie/빈하트.png" class="heart-empty">
-              </div>
-            </div>
+      <div class="movie-item" id="${movie.id}">
+        <div class="movie">
+          <a href="${movie.link}">
+            <img src="${movie.imageSrc}" alt="${movie.altText}" class="movie-img">
+          </a>
+          <div class="movie-time">${movie.time}</div>
+        </div>
+        <div class="text-love">
+          <div class="text">
+            <p>${movie.title}</p>
           </div>
-        `;
+          <div class="love">
+            <img src="${movie.heart.emptySrc}" class="heart-empty">
+          </div>
+        </div>
+      </div>
+    `;
     movieList.innerHTML += movieItem;
   });
-}
-
-if (document.getElementById("movieList")) {
-  renderMovieList();
-}
-
-const movieId = getMovieIdFromURL();
-if (movieId) {
-  displayMovieDetails(movieId);
 }
